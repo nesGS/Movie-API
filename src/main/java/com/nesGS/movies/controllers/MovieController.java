57154f2b783253c8ef84214b10dbcca62b783f9c
nesGS <nesgonsua@gmail.com>
2024-10-18 16:27:42 +0100

    feat: primera version, a falta de integracion con la DB

---
+++ src/main/java/com/nesGS/movies/controllers/MovieController.java
@@ -0,0 +1,75 @@
+package com.nesGS.movies.controllers;
+
+import com.nesGS.movies.models.Movie;
+import com.nesGS.movies.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "*")  // Se regula los dominios que pueden acceder ("*" permite cualquiera)
+@RestController
+@RequestMapping("api/movies")
+public class MovieController {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @GetMapping
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getMovieById(@PathVariable Long id) {
+        Optional<Movie> movie = movieRepository.findById(id);
+        return movie.map(ResponseEntity::ok)  // if (movie.isPresent()) { return ResponseEntity.ok(movie.get()); }
+                    .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) {
+        Movie savedMovie = movieRepository.save(movie);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedMovie);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMovie(@PathVariable Long id) {
+        if(!movieRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        movieRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody Movie updatedMovie) {
+        if(!movieRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        updatedMovie.setId(id);
+        Movie savedMovie = movieRepository.save(updatedMovie);
+        return ResponseEntity.ok(savedMovie);
+    }
+
+    @GetMapping("/vote/{id}/{rating}")
+    public ResponseEntity<Movie> voteMovie(@PathVariable Long id, @PathVariable double rating) {
+        if(!movieRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        Optional<Movie> optional = movieRepository.findById(id);
+        Movie movie = optional.get();
+
+        double newRating = ((movie.getVotes() * movie.getRating()) + rating) / (movie.getVotes() + 1);
+
+        movie.setVotes(movie.getVotes() + 1);
+        movie.setRating(newRating);
+
+        Movie savedMovie = movieRepository.save(movie);
+        return ResponseEntity.ok(savedMovie);
+    }
+}
+
