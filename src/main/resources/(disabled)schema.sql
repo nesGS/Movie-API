f8512cd895fa96bef9c9b1f4ab0024c57dcb22a5
nesGS <nesgonsua@gmail.com>
2024-11-02 22:50:36 +0000

    Se añade script sql y se modifica .properties

---
+++ src/main/resources/(disabled)schema.sql
@@ -0,0 +1,8 @@
+--CREATE TABLE IF NOT EXISTS Movie (
+--    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+--    title VARCHAR(255) NOT NULL,
+--    releaseYear INT NOT NULL,
+--    votes INT NOT NULL,
+--    rating DOUBLE NOT NULL,
+--    image_url VARCHAR(255)
+--);
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Movies (id, title, release_year, votes, rating, image_url) VALUES
+(1, 'Inception', 2010, 2000000, 8.8, 'https://example.com/inception.jpg'),
+(2, 'The Matrix', 1999, 1700000, 8.7, 'https://example.com/thematrix.jpg'),
+(3, 'Interstellar', 2014, 1500000, 8.6, 'https://example.com/interstellar.jpg'),
+(4, 'Parasite', 2019, 1000000, 8.6, 'https://example.com/parasite.jpg'),
+(5, 'The Dark Knight', 2008, 2300000, 9.0, 'https://example.com/thedarkknight.jpg');
